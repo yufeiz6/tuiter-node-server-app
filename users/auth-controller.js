737fdba7e5ea4485a2ce49e16413d185b59a7b24
yufeiz6 <yufeizhang0718@gmail.com>
2023-06-19 18:53:58 -0700

    fix

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -8,7 +8,7 @@ const AuthController = (app) => {
       res.sendStatus(403);
       return;
     }
-    const newUser = await userDao.createUser(req.body);
+    const newUser = await usersDao.createUser(req.body);
     req.session["currentUser"] = newUser;
     res.json(newUser);
   };
